85008cfc79eb2a70d8a66c0e6436f54e7d868e31
CrazyChaos <s-kempinger@gmx.at>
2018-03-18 11:54:22 +0100

    Creation of AiPlayer & Human Player

---
+++ Server/src/A_Single_Game.java
@@ -0,0 +1,15 @@
+public class A_Single_Game {
+
+    private Player player;
+    private int[][] arena;
+    private int[][] currTrock;
+    private int[][] nextTrock;
+    private int currTrockX;
+    private int currTrockY;
+
+    public A_Single_Game() {
+        this.arena = new int[20][10];
+        this.currTrock = currTrock;
+        this.nextTrock = nextTrock;
+    }
+}
