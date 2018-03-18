81f4e6b69e9ad81155374721ed0c38c9ed1f3980
CrazyChaos <s-kempinger@gmx.at>
2018-03-18 11:58:47 +0100

    JUNIT NOT WORKI ON MY PC :(

--- Server/src/at/dropical/server/gamefield/TetrominoTypes.java
+++ Server/src/at/dropical/server/gamefield/TetrominoTypes.java
@@ -8,36 +8,47 @@ import java.util.Random;
  * I, T, L, J, Z, S, O
  * For selecting a random one, use random().
  * Additionally there is an empty type.
- *
+ * <p>
  * The Tetrominos are an Array of the different
  * rotations of one Tetromino.<br>
  * <code>
- *     int[][] rotatedTetr = Imatrix[0]<br>
+ * int[][] rotatedTetr = Imatrix[0]<br>
  * </code>
  * rotatedTetr has the size {@link Tetromino#size}<br>
- *
+ * <p>
  * I am not pleased with how this is implemented,
  * but I didn't know how to do this with enums.
  * The autoformatting of IntelliJ is messing
  * up here a little bit.
  */
 public class TetrominoTypes {
-    /** How many types there are. Not counting an empty one.
-     * Has to be updated manually if code changes! */
+    /**
+     * How many types there are. Not counting an empty one.
+     * Has to be updated manually if code changes!
+     */
     public static final int count = 7;
 
-    /** Returns a randomly selected type. */
+    /**
+     * Returns a randomly selected type.
+     */
     public static int[][][] random() {
         // will be between 0 (inclusive) and the specified value (exclusive)
         int rand = new Random().nextInt(count);
-        switch(rand) {
-            case 0: return Imatrix;
-            case 1: return Tmatrix;
-            case 2: return Lmatrix;
-            case 3: return Jmatrix;
-            case 4: return Zmatrix;
-            case 5: return Smatrix;
-            case 6: return Omatrix;
+        switch (rand) {
+            case 0:
+                return Imatrix;
+            case 1:
+                return Tmatrix;
+            case 2:
+                return Lmatrix;
+            case 3:
+                return Jmatrix;
+            case 4:
+                return Zmatrix;
+            case 5:
+                return Smatrix;
+            case 6:
+                return Omatrix;
             // Should never happen!
             default: {
                 System.err.println("The Randomiser in TetrominoTypes.random() does not work!");
@@ -58,7 +69,8 @@ public class TetrominoTypes {
             {0, 1, 0, 0},
             {0, 1, 0, 0},
             {0, 1, 0, 0}
-    }};
+    }
+    };
     //TTTTTTTTTTTTTTTTTTTTTTTTTTTT
     public static final int[][][] Tmatrix = {
             {
@@ -161,7 +173,7 @@ public class TetrominoTypes {
                     {0, 7, 7, 0},
                     {0, 0, 0, 0},
                     {0, 0, 0, 0}
-    }};
+            }};
 
 
     public static final int[][][] emptyMatrix = {
