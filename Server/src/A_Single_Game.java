09465079ca2b4effa601e3025199051a570048d0
CrazyChaos <s-kempinger@gmx.at>
2018-03-18 12:06:41 +0100

    anpassungen

--- Server/src/A_Single_Game.java
+++ Server/src/A_Single_Game.java
@@ -1,14 +1,19 @@
+import at.dropical.server.gamefield.TetrisArena;
+import at.dropical.server.gamefield.Tetromino;
+
+
+//TODO: Besseren namen geben
 public class A_Single_Game {
 
     private Player player;
-    private int[][] arena;
-    private int[][] currTrock;
+    private TetrisArena arena;
+    private Tetromino currTrock;
     private int[][] nextTrock;
     private int currTrockX;
     private int currTrockY;
 
     public A_Single_Game() {
-        this.arena = new int[20][10];
+        this.arena = new TetrisArena();
         this.currTrock = currTrock;
         this.nextTrock = nextTrock;
     }
