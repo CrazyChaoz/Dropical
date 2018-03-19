7f0c6c9f46ab6582ba9b3ecd7f479adf286ea5f8
Julian Wollersberger <julian.derzocker@gmx.at>
2018-03-19 09:12:56 +0100

    Copied Multi- and SinglePlayerManager

---
+++ AI/src/module-info.java
@@ -0,0 +1,13 @@
+// Created by julian on 18.03.18.
+/**
+ * Normally, you can play Tetris on your own or against an other player.
+ * This is boring. One needs a Super <b>Artificial Intelligence</b> that
+ * is better than any human!
+ *
+ * <H1>Brute Force Search Tree</H1>
+ * As a first attempt I'm making a brute force algorithm
+ * that tries all possibilities where to place the tetrominos.
+ * The best option will be taken.
+ */
+module AI {
+}
--- src/at/dropical/server/gamefield/TetrisArena.java
+++ src/at/dropical/server/gamefield/TetrisArena.java
@@ -200,7 +200,10 @@ public class TetrisArena {
      * This is for multiplayer.
      * Maybe add a mode with this in singleplayer too?
      * @return wherever a block got over the top of
-     * the arena. True -> game over!*/
+     * the arena. True -> game over!
+     *
+     * fixme The current Tetroino must go up too,
+     * otherwise it can lead to a wrong game over. */
     public boolean addLines(int lineCount) {
         boolean overTop = false;
         Random rand = new Random();
