a06db54c115648c7800eb1ff17bd277a591cc028
CrazyChaos <s-kempinger@gmx.at>
2018-03-31 23:27:20 +0200

    created gameOverState

--- src/at/dropical/server/gamestates/GameOverState.java
+++ src/at/dropical/server/gamestates/GameOverState.java
@@ -1,4 +1,17 @@
 package at.dropical.server.gamestates;
 
-public class GameOverState {
+import at.dropical.server.game.Game;
+import at.dropical.shared.net.requests.GameDataContainer;
+import at.dropical.shared.net.requests.InputDataContainer;
+
+public class GameOverState implements State{
+    @Override
+    public GameDataContainer fillGameDataContainer(Game game, GameDataContainer gameDataContainer) {
+        return null;
+    }
+
+    @Override
+    public void handleInput(Game game, InputDataContainer inputDataContainer, int playerNumber) {
+
+    }
 }
