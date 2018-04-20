385ecfe3158dfdf2cfcb5b18145445f5a99de3fd
CrazyChaos <s-kempinger@gmx.at>
2018-04-20 12:03:44 +0200

    doing stuff

---
+++ client/src/at/dropical/client/ClientSideTransmitter.java
@@ -0,0 +1,41 @@
+package at.dropical.client;
+
+import at.dropical.shared.net.requests.Request;
+import at.dropical.shared.net.transmitter.Transmitter;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientSideTransmitter implements Transmitter {
+
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+
+    public ClientSideTransmitter(Socket socket){
+        try {
+            inputStream=new ObjectInputStream(socket.getInputStream());
+            outputStream=new ObjectOutputStream(socket.getOutputStream());
+        }catch (IOException e){}
+    }
+
+    @Override
+    public void writeRequest(Request r) {
+        try {
+            outputStream.writeObject(r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Request readRequest() throws IOException {
+        try {
+            return (Request) inputStream.readObject();
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
