f84af54d7799e69a77174c82afd3de5c62bccee7
CrazyChaos <s-kempinger@gmx.at>
2018-04-22 10:47:36 +0200

    refactoring & debugging

--- client/src/at/dropical/client/BestClientOfAllTime.kt
+++ client/src/at/dropical/client/BestClientOfAllTime.kt
@@ -1,66 +1,30 @@
 import at.dropical.client.Proxy
-import at.dropical.shared.PlayerAction
-import at.dropical.shared.net.requests.*
-import java.io.IOException
+import at.dropical.shared.net.requests.CreateGameRequest
+import at.dropical.shared.net.requests.JoinRequest
+import at.dropical.shared.net.requests.ListRequest
 import java.net.Socket
 import java.util.*
 
-object BestClientOfAllTime {
-    private val scanner = Scanner(System.`in`)
-    private val input: PlayerAction
-        get() {
-            val retval: PlayerAction? = null
-            while (retval == null) {
-                when (scanner.next()) {
-                    "w" -> return PlayerAction.UP
-                    "a" -> return PlayerAction.LEFT
-                    "s" -> return PlayerAction.DOWN
-                    "d" -> return PlayerAction.RIGHT
-                    " " -> return PlayerAction.DROP
-                }
+fun main(args: Array<String>) {
+    val proxy = Proxy(Socket("localhost", 45000))
+    val scanner = Scanner(System.`in`)
+
+    var i = 1
+    while (i != 0) {
+        println("What do you want to do?")
+        println("[1]: list currently open games")
+        println("[2]: create new game")
+        println("[3]: join a game")
+        i = scanner.nextInt()
+        when (i) {
+            1 -> proxy.transmitToServer(ListRequest(true))
+            2 -> {
+                println("What should this game be called?")
+                proxy.transmitToServer(CreateGameRequest(scanner.next()))
             }
-            return retval
-        }
-
-    @Throws(IOException::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val proxy = Proxy(Socket("localhost", 45000))
-        val playername: String
-
-        println("Whats your name?")
-        playername = scanner.next()
-
-
-        var i = 1
-        while (i != 0) {
-            println("So, $playername, what do you want to do?")
-            println("[1]: list currently open games")
-            println("[2]: create new game")
-            println("[3]: join a game")
-            println("[4]: list connected players to your game")
-            println("[5]: start a game")
-            println("[6]: send input to the server")
-            i = scanner.nextInt()
-            when (i) {
-                1 -> proxy.transmitToServer(ListGamesRequest())
-                2 -> {
-                    println("What should this game be called?")
-                    proxy.transmitToServer(CreateGameRequest(scanner.next()))
-                }
-                3 -> {
-                    println("Which game do you want to join?")
-                    proxy.transmitToServer(JoinRequest(scanner.next(), playername))
-                }
-                4 -> proxy.transmitToServer(ListPlayersRequest())
-                5 -> {
-                    println("Which game do you want to start?")
-                    proxy.transmitToServer(StartGameRequest(scanner.next()))
-                }
-                6 -> {
-                    println("What Input Key do you want to send?")
-                    proxy.transmitToServer(HandleInputRequest(playername, input))
-                }
+            3 -> {
+                println("Which game do you want to join?")
+                proxy.transmitToServer(JoinRequest(scanner.next()))
             }
         }
     }
--- client/src/at/dropical/client/BestJavaClient.java
+++ client/src/at/dropical/client/BestJavaClient.java
@@ -1,36 +1,28 @@
 package at.dropical.client;
 
-import at.dropical.shared.PlayerAction;
-import at.dropical.shared.net.requests.*;
+import at.dropical.shared.net.requests.CreateGameRequest;
+import at.dropical.shared.net.requests.JoinRequest;
+import at.dropical.shared.net.requests.ListRequest;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class BestJavaClient {
-    private static Scanner scanner=new Scanner(System.in);
-
     public static void main(String[] args) throws IOException {
         Proxy proxy=new Proxy(new Socket("localhost",45000));
-        String playername;
-
-        System.out.println("Whats your name?");
-        playername=scanner.next();
-
+        Scanner scanner=new Scanner(System.in);
 
         int i=1;
         while (i!=0){
-            System.out.println("So, "+playername+", what do you want to do?");
+            System.out.println("What do you want to do?");
             System.out.println("[1]: list currently open games");
             System.out.println("[2]: create new game");
             System.out.println("[3]: join a game");
-            System.out.println("[4]: list connected players to your game");
-            System.out.println("[5]: start a game");
-            System.out.println("[6]: send input to the server");
             i=scanner.nextInt();
             switch (i){
                 case 1:
-                    proxy.transmitToServer(new ListGamesRequest());
+                    proxy.transmitToServer(new ListRequest(true));
                     break;
                 case 2:
                     System.out.println("What should this game be called?");
@@ -38,39 +30,9 @@ public class BestJavaClient {
                     break;
                 case 3:
                     System.out.println("Which game do you want to join?");
-                    proxy.transmitToServer(new JoinRequest(scanner.next(),playername));
-                    break;
-                case 4:
-                    proxy.transmitToServer(new ListPlayersRequest());
-                    break;
-                case 5:
-                    System.out.println("Which game do you want to start?");
-                    proxy.transmitToServer(new StartGameRequest(scanner.next()));
-                    break;
-                case 6:
-                    System.out.println("What Input Key do you want to send?");
-                    proxy.transmitToServer(new HandleInputRequest(playername,getInput()));
+                    proxy.transmitToServer(new JoinRequest(scanner.next()));
                     break;
             }
         }
-    }
-    private static PlayerAction getInput(){
-        PlayerAction retval=null;
-        while (retval==null){
-            switch (scanner.next()){
-                case "w":
-                    return PlayerAction.UP;
-                case "a":
-                    return PlayerAction.LEFT;
-                case "s":
-                    return PlayerAction.DOWN;
-                case "d":
-                    return PlayerAction.RIGHT;
-                case " ":
-                    return PlayerAction.DROP;
-
-            }
-        }
-        return retval;
     }
 }
