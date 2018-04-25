6a485f6df247165d720909107aea1bb9ba666bdf
CrazyChaos <s-kempinger@gmx.at>
2018-04-25 18:17:33 +0200

    added the option to create a game with a specific number of players

--- shared/src/at/dropical/shared/net/requests/CreateGameRequest.java
+++ shared/src/at/dropical/shared/net/requests/CreateGameRequest.java
@@ -4,12 +4,22 @@ import at.dropical.shared.net.abstracts.Request;
 
 public class CreateGameRequest implements Request {
     private String gameName;
+    private int maxPlayers;
 
     public CreateGameRequest(String gameName) {
         this.gameName = gameName;
+        this.maxPlayers=2;
+    }
+    public CreateGameRequest(String gameName,int maxPlayers) {
+        this.gameName = gameName;
+        this.maxPlayers=maxPlayers;
     }
 
     public String getGameName() {
         return gameName;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
     }
 }
